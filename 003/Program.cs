a5919acffe2d538440c6063d2292677aa68d2b42
Cat <ligeia_undead@mail.ru>
2023-02-13 18:11:22 +0300

    Поиск индекса заданного числа  с помощью методов и массивов

---
+++ 003/Program.cs
@@ -0,0 +1,19 @@
+﻿// Поиск максимума из 9 с помощью методов и массивов
+int[] array = {1,12,13,31,6,8,15,201};
+
+int n = array.Length;
+int find = 13;
+
+int index = 0;
+
+while(index<n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    //index = index+1;
+    index++;
+}
+
+
